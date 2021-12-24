62cb31be22241b48d48b10c2f23ab6000bbdb70c
rodennis <rev.rltodd@gmail.com>
2021-12-23 23:02:16 -0600

    add responsive css

--- client/src/components/Projects/Projects.jsx
+++ client/src/components/Projects/Projects.jsx
@@ -30,7 +30,7 @@ function Projects() {
           <img className='project1' src={ pain } alt="project" />
         </a>
           <div className='tech-stack-div'>
-            <p className='project-info'>PAIN is an effective, to the point workout app. In the age of millions of apps on the web, ranging from overcomplicated, to not useful at all. PAIN is a straight to the point kind of app. You are given the choice to start a session, you will then provide what workouts you did that session. You will include, but not limited to, the exercise name, the weight, the amount of reps, sets and, the RPE. Leveraging the airtable api, so that you are able to edit, delete, or reflect on later the workouts you have completed.</p>
+            <p className='project-info project-info-mobile'>PAIN is an effective, to the point workout app. In the age of millions of apps on the web, ranging from overcomplicated, to not useful at all. PAIN is a straight to the point kind of app. You are given the choice to start a session, you will then provide what workouts you did that session. You will include, but not limited to, the exercise name, the weight, the amount of reps, sets and, the RPE. Leveraging the airtable api, so that you are able to edit, delete, or reflect on later the workouts you have completed.</p>
                 <img src={react} alt="react" />
                 <img src={ css } alt="css" />
                 <img src={ mui } alt="mui" />
--- client/src/components/Socials/Socials.jsx
+++ client/src/components/Socials/Socials.jsx
@@ -9,7 +9,7 @@ function Socials() {
     <div>
       <a target='_blank' rel="noreferrer" href="https://www.linkedin.com/in/rodney-todd-a676b4128/" ><img className='socials' src={linked} alt="linkedin"/></a>
       <a target='_blank' rel="noreferrer" href="https://github.com/rodennis?tab=repositories"><img className='socials' src={ github } alt="github" /></a>
-      <a target='_blank' rel="noreferrer" href="https://www.overleaf.com/read/xvhmqcpvqxnc"><img className='socials' src={ resume } alt="resume"/></a>
+      <a target='_blank' rel="noreferrer" href="https://www.canva.com/design/DAEzSp9zZFY/Nn6ECijJGuoda6rWFTtYlQ/view?utm_content=DAEzSp9zZFY&utm_campaign=designshare&utm_medium=link&utm_source=homepage_design_menu"><img className='socials' src={ resume } alt="resume"/></a>
     </div>
   )
 }
