7694763973da0aff96a408849178d9e3dda41b7a
rodennis <rev.rltodd@gmail.com>
2022-01-19 20:22:46 -0600

    update resume link

--- client/src/components/CallToAction/CallToAction.jsx
+++ client/src/components/CallToAction/CallToAction.jsx
@@ -1,35 +1,43 @@
-import React from 'react'
-import './CallToAction.css'
-import { Link } from 'react-router-dom' 
-import Selfie from '../Photos/self.jpg'
-import Socials from '../Socials/Socials'
-  
+import React from "react";
+import "./CallToAction.css";
+import { Link } from "react-router-dom";
+import Selfie from "../Photos/self.jpg";
+import Socials from "../Socials/Socials";
+
 function CallToAction() {
   return (
     <div>
-        <p className='top-intro'>RODNEY .T</p>
-        <p className='bottom-intro'>SOFTWARE ENGINEER</p> 
-      <div className='about'>
-        <div className='photo'>
-          <img className='selfie' src={ Selfie } alt="" />
+      <p className="top-intro">RODNEY .T</p>
+      <p className="bottom-intro">SOFTWARE ENGINEER</p>
+      <div className="about">
+        <div className="photo">
+          <img className="selfie" src={Selfie} alt="" />
         </div>
-        <div className='about-me'>
-          <h1 className='about-title'>About</h1>
-        <p>
-          Hey there, welcome to my Portfolio, my name is Rodney Todd. I am a full stack web developer, who enjoys every aspect of coding! One day I was sitting on my couch browsing the app store, and thought "hmm it would be interesting to build my own app". Ever since that day I have pursued web development.  
-        </p>
-        <p>
-          I love the easy times of coding, but I also love the times when you're so frustrated with some code that you could throw your computer. There is just something about it, when you finally find a solution to your problem, and can sit back and look at the finished work!
-        </p>
-        <p>
-          Hit that Button and let's talk!
-        </p>
-        <Link className='Link' to='/contact'><button className='contact-link'> Contact Me!</button></Link>
+        <div className="about-me">
+          <h1 className="about-title">About</h1>
+          <p>
+            Hey there, welcome to my Portfolio, my name is Rodney Todd. I am a
+            full stack web developer, who enjoys every aspect of coding! One day
+            I was sitting on my couch browsing the app store, and thought "hmm
+            it would be interesting to build my own app". Ever since that day I
+            have pursued web development.
+          </p>
+          <p>
+            I love the easy times of coding, but I also love the times when
+            you're so frustrated with some code that you could throw your
+            computer. There is just something about it, when you finally find a
+            solution to your problem, and can sit back and look at the finished
+            work!
+          </p>
+          <p>Hit that Button and let's talk!</p>
+          <Link className="Link" to="/contact">
+            <button className="contact-link"> Contact Me!</button>
+          </Link>
           <Socials />
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default CallToAction
+export default CallToAction;
--- client/src/components/Socials/Socials.jsx
+++ client/src/components/Socials/Socials.jsx
@@ -9,7 +9,7 @@ function Socials() {
     <div>
       <a target='_blank' rel="noreferrer" href="https://www.linkedin.com/in/rodney-todd-a676b4128/" ><img className='socials' src={linked} alt="linkedin"/></a>
       <a target='_blank' rel="noreferrer" href="https://github.com/rodennis?tab=repositories"><img className='socials' src={ github } alt="github" /></a>
-      <a target='_blank' rel="noreferrer" href="https://www.canva.com/design/DAEzSp9zZFY/Nn6ECijJGuoda6rWFTtYlQ/view?utm_content=DAEzSp9zZFY&utm_campaign=designshare&utm_medium=link&utm_source=homepage_design_menu"><img className='socials' src={ resume } alt="resume"/></a>
+      <a target='_blank' rel="noreferrer" href="https://www.canva.com/design/DAE1uVivQGQ/i0wd2_FIe1tD5_ToCuaqFA/view?utm_content=DAE1uVivQGQ&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink"><img className='socials' src={ resume } alt="resume"/></a>
     </div>
   )
 }
