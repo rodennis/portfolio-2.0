696ea5d1aaa27f83a205a3c481fcc948d08f9fcf
rodennis <rev.rltodd@gmail.com>
2022-02-14 21:30:33 -0600

    add new project

--- client/src/components/Projects/Projects.jsx
+++ client/src/components/Projects/Projects.jsx
@@ -4,10 +4,7 @@ import { useInView } from 'react-intersection-observer';
 import react from '../Photos/react.png'
 import css from '../Photos/css.png'
 import pain from '../Photos/pain.png'
-import mui from '../Photos/mui.png'
-import html from '../Photos/html-5.png'
-import javascript from '../Photos/js-file.png'
-import etchASketch from '../Photos/etch-a-sketch.png'
+import hcfreviews from '../Photos/hcf-reviews.png'
 import node from '../Photos/nodejs.png'
 import RNLH from '../Photos/RNLH.png'
 import mongoose from '../Photos/mongoose.png'
@@ -28,34 +25,31 @@ function Projects() {
       </div>
       <div className="projects">
         <div className={inView ? "project-inview zoom" : "project-inview"} ref={ref}>
-        <a href="https://pain-workout.netlify.app/">
+        <a href="https://pain-workout.netlify.app/" target="_blank" rel="noreferrer">
           <img className='project1' src={ pain } alt="project" />
         </a>
           <div className='tech-stack-div'>
             <p className='project-info project-info-mobile'>PAIN is an effective, to the point workout app. In the age of millions of apps on the web, ranging from overcomplicated, to not useful at all. PAIN is a straight to the point kind of app. You are given the choice to start a session, you will then provide what workouts you did that session. You will include, but not limited to, the exercise name, the weight, the amount of reps, sets and, the RPE. Using Nodejs for the backend along with Express, Mongoose, and MongoDB for the database.</p>
                 <img src={react} alt="react" />
                 <img src={ css } alt="css" />
-                <img src={mui} alt="mui" />
                 <img src={node} alt="node" />
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original-wordmark.svg" alt="mongoDB" />
           </div>
         </div>
         <div className={inView ? "project-inview zoom" : "project-inview"} ref={ref}>
-        <a href="https://rodennis.github.io/etch-a-sketch-1/">
-          <img className='project1' src={ etchASketch } alt="project" />
+        <a href="https://hcf-reviews.netlify.app/" target="_blank" rel="noreferrer">
+          <img className='project1' src={ hcfreviews } alt="project" />
         </a>
           <div className='tech-stack-div'>
-            <p className='project-info'>This is a vanilla javascript, html, css project, where i was able to leverage javascript to create a dynamic game of etch-a-sketch. The player is able to choose from a black or random color, choose the size of the board to draw on, and draw whatever their imagination comes up with!</p>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <img src={ html } alt="html" />
+            <p className='project-info'>HCF-Review is a website for healthcare professionals to go and write truthful reviews about a healthcare facility they have worked at. A user will be able to search for a healthcare facility. Where they can rate the hospital, Share salary information, or answer other reviewer questions. They can be anonymouse or add their personal information if they would like. If a healthcare facility is not present on the website, the user will be able to add it and then write a review.</p>
+            <img src={ react } alt="html" />
             <img src={ css } alt="css" />
-            <img src={ javascript } alt="javascript" />
+            <img src={ node } alt="node" />
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original-wordmark.svg" alt="mongoDB" />
           </div>
         </div>
         <div className={inView ? "project-inview zoom" : "project-inview"} ref={ref}>
-          <a href="https://github.com/rodennis/RNLH">
+          <a href="https://rnlh.netlify.app/" target="_blank" rel="noreferrer">
           <img className='project1' src={ RNLH } alt="project" />
         </a>
             <div className='tech-stack-div'>
