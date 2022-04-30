aa4d9bc5ebda107c8f217acda81b1f165bf518b9
rodennis <rev.rltodd@gmail.com>
2022-04-30 14:17:59 -0500

    update resume link

--- client/src/components/Socials/Socials.jsx
+++ client/src/components/Socials/Socials.jsx
@@ -9,7 +9,7 @@ function Socials() {
     <div>
       <a target='_blank' rel="noreferrer" href="https://www.linkedin.com/in/rodney-todd-a676b4128/" ><img className='socials' src={linked} alt="linkedin"/></a>
       <a target='_blank' rel="noreferrer" href="https://github.com/rodennis?tab=repositories"><img className='socials' src={ github } alt="github" /></a>
-      <a target='_blank' rel="noreferrer" href="https://drive.google.com/file/d/1M2RmbdAH8xpC1iuMPJd4tvPr-PQp0HfP/view?usp=sharing"><img className='socials' src={ resume } alt="resume"/></a>
+      <a target='_blank' rel="noreferrer" href="https://docs.google.com/document/d/1b4Jj8uvE5TlvR6NLgrkrlFxvROhyr8YC/edit?usp=sharing&ouid=113518055476624147368&rtpof=true&sd=true"><img className='socials' src={ resume } alt="resume"/></a>
     </div>
   )
 }
